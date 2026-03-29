0efa4fdd6b77cdd89d8be42ba81ec517e57e2fc8
alex.rodriguez.r <alex.542004@gmail.com>
2026-03-30 00:08:16 +0200

    Add submission packaging script to create a zip file for upload

---
+++ make_submission.py
@@ -0,0 +1,30 @@
+import os
+import zipfile
+
+
+def main():
+    print("Packing it all up...")
+    submission_filename = "submission.zip"
+
+    ignore_dirs = {"data", "__pycache__"}
+
+    with zipfile.ZipFile(submission_filename, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk("."):
+            dirs[:] = [
+                d for d in dirs if not d.startswith(".") and d not in ignore_dirs
+            ]
+
+            for file in files:
+                if file.startswith(".") or file == submission_filename:
+                    continue
+
+                file_path = os.path.join(root, file)
+                archive_path = os.path.relpath(file_path, ".")
+
+                zipf.write(file_path, arcname=archive_path)
+
+    print(f"\n'{submission_filename}' is ready for upload!")
+
+
+if __name__ == "__main__":
+    main()
